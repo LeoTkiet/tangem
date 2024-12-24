cebc2fc5a6a3d061026e8e9904f4497999a8a673
Huỳnh Tuấn Kiệt <24120356@student.hcmus.edu.vn>
2024-12-24 10:44:45 +0700

    save

--- save_message.php
+++ save_message.php
@@ -1,15 +1,11 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Lấy dữ liệu JSON từ yêu cầu
+    echo "Đã nhận yêu cầu POST!";
     $data = json_decode(file_get_contents('php://input'), true);
-
     if (isset($data['message'])) {
         $message = $data['message'];
         $file = 'messages.txt';
-
-        // Ghi tin nhắn vào file
         file_put_contents($file, $message . "\n", FILE_APPEND);
-
         echo "Tin nhắn đã được lưu thành công.";
     } else {
         http_response_code(400);
